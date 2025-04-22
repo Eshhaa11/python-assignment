a1868cd13e6e4cc0b8678da371fbfac65d33f035
Esha Patel <Patellesha11@gmail.com>
2025-04-22 12:10:40 +0300

    added

---
+++ Q3.py
@@ -0,0 +1,18 @@
+def from_n_to_zero(n):
+    if n == 0:
+        print("already Zero")
+    elif n > 0:
+        n = n - 1
+        while n >= 0:
+            print(n, end=' ')
+            n = n - 1
+        print()
+    else:
+        while n <= 0:
+            print(n, end=' ')
+            n = n + 1
+        print()
+
+from_n_to_zero(0)   
+from_n_to_zero(4)   
+from_n_to_zero(-3)  
